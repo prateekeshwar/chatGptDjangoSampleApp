64b3b7b2f9e92832b36a106021f40b91a3167037
Prateekeshwar <prateek@roundfinance.com>
2023-07-19 20:06:03 +0530

    removed print

--- chatGptChatbot/chatbot/utils.py
+++ chatGptChatbot/chatbot/utils.py
@@ -1,20 +1,23 @@
 import openai
 import spacy
 
-open_ai_api_key = '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'
+open_ai_api_key = 'INPUT_YOUR KEY'
 openai.api_key = open_ai_api_key
 
 def ask_open_ai(message):
-    response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = message,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-    answer = response.choices[0].text.strip()
-    return answer
+    try:
+        response = openai.Completion.create(
+            model = "text-davinci-003",
+            prompt = message,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.7
+        )
+        answer = response.choices[0].text.strip()
+        return answer
+    except Exception as e:
+        return str(e)
 
 def get_search_terms(question):
     nlp = spacy.load("en_core_web_sm")
